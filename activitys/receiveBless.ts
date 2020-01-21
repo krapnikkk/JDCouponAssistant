98d84d3ca1b2a7e653777ae86956a270ec0b31b1
krapnik <krapnik@qq.com>
2020-01-21 15:06:12 +0800

    update ReceiveBless

--- activitys/receiveBless.ts
+++ activitys/receiveBless.ts
@@ -4,23 +4,32 @@ import Config from "../config/config";
 
 export default class ReceiveBless implements Activity {
     url: string = "https://api.m.jd.com/client.action";
+    detailurl: string = "https://krapnik.cn/api/bless/list";
+    sendUrl: string = "https://krapnik.cn/api/bless/register";
     params: any;
     data: any;
     container: HTMLDivElement;
     outputTextarea: HTMLTextAreaElement;
     shareLink: string = "";
+    shareData: Array<string> = [];
     constructor(params: any, containerDiv: HTMLDivElement, outputTextarea: HTMLTextAreaElement) {
         this.params = params;
         this.container = containerDiv;
         this.outputTextarea = outputTextarea;
+        this.outputTextarea.value = `当你看到这行文字时，说明你还没有登录京东帐号！`;
     }
     get(): void {
         fetch("https://api.m.jd.com/client.action?functionId=getDefaultTpl&body={%22templateId%22:1001}&appid=content_ecology&clientVersion=1.0.0&client=wh5", { credentials: "include" })
             .then((res) => { return res.json() })
             .then((json) => {
-                var data = json.result;
-                this.shareLink = `https://bunearth.m.jd.com/babelDiy/ZAEYNFNMBSWTXROQWKCR/21tFbS6Xm4tpon3oJnwzbnCJBo1Z/index.html?ad_od=1&DDid=${data.uuid}&ukey=${data.ukey}&modelid=${data.templateId}&go=CARD`;
-                this.list();
+                if (json.code !== "60001") {
+                    this.data = json.result;
+                    this.shareLink = `https://bunearth.m.jd.com/babelDiy/ZAEYNFNMBSWTXROQWKCR/21tFbS6Xm4tpon3oJnwzbnCJBo1Z/index.html?ad_od=1&DDid=${this.data.uuid}&ukey=${this.data.ukey}&modelid=${this.data.templateId}&go=CARD`;
+                    this.list();
+                    this.shareList();
+                    this.outputTextarea.value = "";
+                }
+
             })
     }
 
@@ -28,24 +37,29 @@ export default class ReceiveBless implements Activity {
         const content = document.createElement("div");
         let msg = `
         <div style="margin:10px;">
-        <button class="copy" style="width: 200px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">复制我的分享链接</button>
-        <button class="share" style="width: 120px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">分享我的拜年链接</button>
-        <input class="inviteLink" type="text" style="width:80vw;height: 25px;font-size:14px;border: solid 1px #000;border-radius: 5px;margin: 10px auto;display: block;" placeholder="请输入需要助力的分享链接">
-        <button class="assist" style="width: 120px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">为TA助力</button>
-        <button class="assistAll" style="width: 120px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">一键为分享库助力</button>
+        <a href="https://gitee.com/krapnik/res/raw/master/Bless.png" target="_blank">点击打开新窗口查看活动攻略</a>
+        <button class="copy" style="width: 200px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">复制我的拜年链接</button>
+        <button class="share" style="width: 200px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">提交我的拜年链接</button>
+        <input class="inviteLink" type="text" style="width:80vw;height: 25px;font-size:14px;border: solid 1px #000;border-radius: 5px;margin: 10px auto;display: block;" placeholder="请输入需要助力的拜年链接">
+        <button class="assist" style="width: 200px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">为TA助力</button>
+        <button class="assistAll" style="width: 200px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block">一键为拜年库数据拜年</button>
+        <p style="font-size:14px;color:red;">一键为拜年库的前50个数据拜年<br>【提交时间倒序】<br>你也可以将你的拜年链接提交上去<br>复制拜年链接后设置京东UA再打开可查看活动首页</p>
         </div>`;
-
         content.innerHTML = msg;
         this.container.appendChild(content);
         const h = _$('.home'),
             c = _$('.copy'),
-            g = _$('.browse'),
-            invite = _$('.inviteLink'),
+            s = _$('.share'),
             assist = _$('.assist'),
-            a = _$('.auto');
+            a = _$('.assistAll');
 
         c!.addEventListener('click', () => {
             Utils.copyText(this.shareLink);
+        });
+
+        a!.addEventListener('click', () => {
+            Utils.outPutLog(this.outputTextarea, `开始自动为拜年库数据拜年`)
+            this.assistAll();
         });
 
         assist!.addEventListener('click', () => {
@@ -53,26 +67,96 @@ export default class ReceiveBless implements Activity {
             this.assist(link.value);
         })
 
+        s!.addEventListener('click', () => {
+            Utils.outPutLog(this.outputTextarea, `提交我的数据到拜年库`)
+            this.send();
+        });
+
     }
 
-    assist(url: string) {
-        if (!url && url.includes('modelid') && url.includes('ukey') && url.includes('DDid')) {
+    async assist(url: string) {
+        if (!url || !url.includes('modelid') || !url.includes('ukey') || !url.includes('DDid')) {
             alert("请输入要助力的分享链接或输入正确的分享地址！");
             return;
         }
-        const modelid = Utils.getSearchString(url, "modelid"),
-            ukey = Utils.getSearchString(url, "ukey"),
-            uuid = Utils.getSearchString(url, "DDid"),
-            data = `functionId=guestReceiveBlessing&appid=content_ecology&body={\"modelid\":${modelid},\"uuid\":\"${uuid}\",\"ukey\":\"${ukey}\"}&client=wh5&clientVersion=1.0.0`;
+        new Promise(resolve => {
+            const modelid = Utils.getSearchString(url, "modelid"),
+                ukey = Utils.getSearchString(url, "ukey"),
+                uuid = Utils.getSearchString(url, "DDid"),
+                data = `functionId=guestReceiveBlessing&appid=content_ecology&body={\"modelid\":${modelid},\"uuid\":\"${uuid}\",\"ukey\":\"${ukey}\"}&client=wh5&clientVersion=1.0.0`;
+            fetch('https://api.m.jd.com/client.action?' + data,
+                {
+                    credentials: "include",
+                }
+            ).then((res) => res.json())
+                .then((json) => {
+                    if(json.code==0){
+                        Utils.outPutLog(this.outputTextarea, `拜年结果：${JSON.stringify(json)}`);
+                    }else{
+                        Utils.outPutLog(this.outputTextarea, `拜年结果：你好像已经给这个人拜过年啦~`);
+                    }
+                    
+                    resolve();
+                });
+        })
+    }
 
-        fetch('https://api.m.jd.com/client.action?' + data,
-            {
-                credentials: "include",
-            }
-        ).then((res) => res.json())
+    async assistAll() {
+        for (let i = 0; i < this.shareData.length; i++) {
+            let url = this.shareData[i];
+            await new Promise(resolve => {
+                setTimeout(async () => {
+                    await this.assist(url);
+                    resolve();
+                }, (Config.timeoutSpan + Utils.random(300, 500)));
+            })
+        }
+        Utils.outPutLog(this.outputTextarea, `自动拜年结束`);
+    }
+
+    shareList(): void {
+        const content = document.createElement("div");
+        content.setAttribute("style", "margin:10px; border:1px solid #000;font-size: 14px;");
+        content.setAttribute("class", "shareList");
+        let msg = "";
+        fetch(this.detailurl).then((res) => { return res.json() })
             .then((json) => {
-                Utils.outPutLog(this.outputTextarea, `助力结果：${json}`);
-            });
+                if (json.success) {
+                    let data = json["data"]["data"];
+                    for (let i = 0; i < data.length; i++) {
+                        let item = data[i],
+                            url = `https://bunearth.m.jd.com/babelDiy/ZAEYNFNMBSWTXROQWKCR/21tFbS6Xm4tpon3oJnwzbnCJBo1Z/index.html?ad_od=1&DDid=${item.DDid}&ukey=${item.ukey}&modelid=${item.modelid}&go=CARD`;
+                        this.shareData.push(url);
+                        msg += `<div style="margin:10px;border-bottom:1px solid #333"><p>id：${item["id"]}<br>DDid：${item["DDid"]}<br>ukey：${item["ukey"]}<br>提交时间：${item["create_time"]}</p>
+                        <button class="help" value="${url}" style="width: 120px;height:30px;background-color: #2196F3;border-radius: 5px;border: 0;color:#fff;margin:5px auto;display:block"">手动拜年</button></div>`
+                    }
+                    content.innerHTML = msg;
+                    this.container.appendChild(content);
+                    _$(".shareList")!.addEventListener("click", (evt) => {
+                        const target = evt.target as HTMLElement;
+                        if (target.tagName == "BUTTON") {
+                            const dataUrl = target!.getAttribute("value")!;
+                            this.assist(dataUrl);
+                        }
+                    })
+                }
+            })
+    }
+
+    send() {
+        if (!this.data) {
+            alert("获取活动数据失败！");
+            return;
+        }
+        let data = `modelid=${this.data["templateId"]}&ukey=${this.data["ukey"]}&DDid=${this.data["uuid"]}`;
+        fetch(this.sendUrl, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+            body: data
+        }).then((res) => { return res.json() })
+            .then((json) => {
+                Utils.outPutLog(this.outputTextarea, JSON.stringify(json));
+            })
     }
 
 }
